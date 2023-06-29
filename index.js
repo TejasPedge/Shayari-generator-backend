cddd2729c281492bb45559bb1945a4d0e7146f45
tejaspedge <tejaspedge512@gmail.com>
2023-06-29 11:57:22 +0530

    done

--- index.js
+++ index.js
@@ -25,39 +25,36 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const generateShayari = async (messages, res) => {
+
+const generateShayari = async (word, res) => {
   try {
+    const messages = [
+      { role: 'system', content: `you are a shayari generator, you generate shayari on the word "${word}" provided by the user` },
+      { role: 'user', content: word }
+    ];
+
     const response = await openai.createChatCompletion({
       model: 'gpt-3.5-turbo',
       messages: messages,
       max_tokens: 100,
     });
-    return response;
+
+    if (response && response.data && response.data.choices && response.data.choices.length > 0) {
+      const shayari = response.data.choices[0].message.content;
+      res.json({ shayari });
+    } else {
+      res.status(500).json({ error: 'Unable to generate shayari' });
+    }
   } catch (error) {
-    res.send({ error: 'Unable to generate shayari' });
+    res.status(500).json({ error: 'Unable to generate shayari' });
     console.error('Error:', error);
   }
 };
 
-const messages = [
-  { role: 'system', content: 'you are a shayari generator, you generates a shayari on word provided by user' },
-];
-
 app.get('/generate-shayari/:word', async (req, res) => {
   try {
     const { word } = req.params;
-    // Logic to generate the Shayari using OpenAI API
-    messages.push({ role: 'user', content: word });
-
-    const response = await generateShayari(messages, res);
-
-    // Send the generated Shayari as the response
-    console.log(response);
-    messages.push(response.data.choices[0].message);
-
-    let shayari = response.data.choices[0].message.content;
-
-    res.json({ shayari });
+    await generateShayari(word, res);
   } catch (error) {
     // Handle any errors that occur during the process
     console.error(error);
